3544c2fe1b0fe150560df6a43433b5d913da54d6
Juan Marulanda Arias <jmaruland@bnl.gov>
2025-07-15 16:32:01 -0400

    Added info messages for users

--- startup/80-proposal_info.py
+++ startup/80-proposal_info.py
@@ -3,7 +3,8 @@
 
 def set_project_name(project_name):
     RE.md['project_name'] = project_name
-
+    current_dir = proposal_path() + f"projects/{project_name}/"
+    print(f'current working at {current_dir}')
 
 # project_name is used by prefect to create the paths inside the porposal directory.
 # The repository that creates the project directories is handled by this repository: https://github.com/NSLS2/opls-workflows
